7551eb1cd09b0ffd1507e4b80c1bf8bda57ce75b
likelet <zhaoqi699@sina.com>
2016-12-23 00:58:16 +0800

    initial commit

--- src/pisseeker/PISseeker.java
+++ src/pisseeker/PISseeker.java
@@ -6,17 +6,63 @@
 
 package pisseeker;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMRecord;
+
 /**
  *
  * @author Administrator
  */
 public class PISseeker {
-
+//    private String gaifile="";
+    private  SAMFileReader srt;
+    private  SAMFileReader src;
+    private HashMap<String,ArrayList<PSIout>> pomap=new HashMap();
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    //tbam and cbam are both sorted bamfile 
+    public PISseeker(String  tbam, String cbam){
+        
+        File bamfile1 = new File(tbam);
+        File bamfile2 = new File(cbam);
+        SAMFileReader srt = new SAMFileReader(bamfile1, new File(bamfile1.getAbsolutePath() + ".bai"));
+        SAMFileReader src = new SAMFileReader(bamfile2, new File(bamfile2.getAbsolutePath() + ".bai"));
     }
-    
+   
+    public void process(){
+        LinkedList<SAMRecord> ls1=new LinkedList<SAMRecord>();
+         LinkedList<SAMRecord> ls2=new LinkedList<SAMRecord>();
+        HashSet<String> hashposition=new HashSet<String>();
+         HashSet<String> chrsome=new HashSet<String>();
+         
+        Iterator iter1 = srt.iterator();
+        Iterator iter2 = src.iterator();
+        
+        while (iter1.hasNext()) {
+            SAMRecord sitem = (SAMRecord) iter1.next();
+            String chrom = sitem.getReferenceName();
+            int start = sitem.getAlignmentStart();
+            int end = sitem.getAlignmentEnd();
+            if (hashposition.add(chrom + end)) {
+                PSIout po = new PSIout(chrom, end, "");
+                if (chrsome.add(chrom)) {
+                    ArrayList<PSIout> psilist = new ArrayList<PSIout>();
+                    psilist.add(po);
+                    pomap.put(chrom, psilist);
+                } else {
+                    pomap.get(chrom).add(po);
+                }
+            }else{
+                int index=pomap.get(chrom).size()-1;//array last one
+                pomap.get(chrom).get(index).add1supporCountInTreat();
+            }       
+        }
+    }
 }
---
+++ src/pisseeker/PSIout.java
@@ -0,0 +1,234 @@
+/*
+ * This class encodes the basic output format of PSI seeker with following columns 
+ chr position base supporCountInTreat totalCountInTreat supporCountControl   totalCountControl Pvalue adjustpvalue
+ */
+
+package pisseeker;
+
+/**
+ *
+ * @author zhaoqi
+ */
+public class PSIout {
+    
+    private String chr;
+    
+    private int position; // 1 based  position
+    
+    private String base; // nuclic acid at candited position
+    
+    private int supporCountInTreat=0;
+    
+    private int totalCountInTreat=0;
+    
+    private int supporCountControl=0;
+    
+    private int totalCountControl = 0;
+    
+    private double Pvalue=1;//fisher test Pvalue
+    
+    private double adjustP = 1;//ajust Pvalue
+
+    public PSIout(String chr, int position, String base) {
+        this.chr = chr;
+        this.position = position;
+        this.base = base;
+    }
+
+    
+    
+    
+    
+    /**
+     * Get the value of adjustP
+     *
+     * @return the value of adjustP
+     */
+    public double getAdjustP() {
+        return adjustP;
+    }
+
+    /**
+     * Set the value of adjustP
+     *
+     * @param adjustP new value of adjustP
+     */
+    public void setAdjustP(double adjustP) {
+        this.adjustP = adjustP;
+    }
+
+
+    /**
+     * Get the value of Pvalue
+     *
+     * @return the value of Pvalue
+     */
+    public double getPvalue() {
+        return Pvalue;
+    }
+
+    /**
+     * Set the value of Pvalue
+     *
+     * @param Pvalue new value of Pvalue
+     */
+    public void setPvalue(double Pvalue) {
+        this.Pvalue = Pvalue;
+    }
+
+
+    /**
+     * Get the value of totalCountControl
+     *
+     * @return the value of totalCountControl
+     */
+    public int getTotalCountControl() {
+        return totalCountControl;
+    }
+
+    /**
+     * Set the value of totalCountControl
+     *
+     * @param totalCountControl new value of totalCountControl
+     */
+    public void setTotalCountControl(int totalCountControl) {
+        this.totalCountControl = totalCountControl;
+    }
+
+    
+
+    /**
+     * Get the value of supporCountControl
+     *
+     * @return the value of supporCountControl
+     */
+    public int getSupporCountControl() {
+        return supporCountControl;
+    }
+
+    /**
+     * Set the value of supporCountControl
+     *
+     * @param supporCountControl new value of supporCountControl
+     */
+    public void setSupporCountControl(int supporCountControl) {
+        this.supporCountControl = supporCountControl;
+    }
+
+
+    /**
+     * Get the value of totalCountInTreat
+     *
+     * @return the value of totalCountInTreat
+     */
+    public int getTotalCountInTreat() {
+        return totalCountInTreat;
+    }
+
+    /**
+     * Set the value of totalCountInTreat
+     *
+     * @param totalCountInTreat new value of totalCountInTreat
+     */
+    public void setTotalCountInTreat(int totalCountInTreat) {
+        this.totalCountInTreat = totalCountInTreat;
+    }
+
+
+    /**
+     * Get the value of supporCountInTreat
+     *
+     * @return the value of supporCountInTreat
+     */
+    public int getSupporCountInTreat() {
+        return supporCountInTreat;
+    }
+
+    /**
+     * Set the value of supporCountInTreat
+     *
+     * @param supporCountInTreat new value of supporCountInTreat
+     */
+    public void setSupporCountInTreat(int supporCountInTreat) {
+        this.supporCountInTreat = supporCountInTreat;
+    }
+
+
+    /**
+     * Get the value of base
+     *
+     * @return the value of base
+     */
+    public String getBase() {
+        return base;
+    }
+
+    /**
+     * Set the value of base
+     *
+     * @param base new value of base
+     */
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+
+    /**
+     * Get the value of position
+     *
+     * @return the value of position
+     */
+    public int getPosition() {
+        return position;
+    }
+
+    /**
+     * Set the value of position
+     *
+     * @param position new value of position
+     */
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    /**
+     * Get the value of chr
+     *
+     * @return the value of chr
+     */
+    public String getChr() {
+        return chr;
+    }
+
+    /**
+     * Set the value of chr
+     *
+     * @param chr new value of chr
+     */
+    public void setChr(String chr) {
+        this.chr = chr;
+    }
+
+    public void add1supporCountInTreat(){
+        this.supporCountInTreat++;
+    }
+    
+    public void add1totalCountInTreat(){
+        this.totalCountInTreat++;
+    }
+    public void add1supporCountControl(){
+        this.supporCountControl++;
+    }
+    public void add1totalCountControl(){
+        this.totalCountControl++;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return chr + "\t" + position + "\t" + base + "\t" + supporCountInTreat + "\t" + totalCountInTreat +"\t" + supporCountControl +"\t" + totalCountControl + "\t" + Pvalue + "\t" + adjustP ;
+    }
+
+    
+    
+}
